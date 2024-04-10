691b72cee9cefe56e8f8d79bcf0f07583ee3e35e
theo <theotraian@yahoo.ro>
2024-04-10 13:24:24 +0300

    User Implementation

--- BoardBloom/BoardBloom/Program.cs
+++ BoardBloom/BoardBloom/Program.cs
@@ -1,4 +1,5 @@
 using BoardBloom.Data;
+using BoardBloom.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
@@ -15,6 +16,13 @@ builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.Requ
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
+
+// SeedData intiliazer
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    SeedData.Initialize(services);
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
