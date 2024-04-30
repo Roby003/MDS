053a8008403cd82134ec0758f83225cfef4e36a5
w-i-l <ocnaru.mihai08@gmail.com>
2024-04-30 16:23:46 +0300

    added bloom view

--- BoardBloom/BoardBloom/Controllers/CommentsController.cs
+++ BoardBloom/BoardBloom/Controllers/CommentsController.cs
@@ -51,9 +51,76 @@ namespace BoardBloom.Controllers
             {
                 TempData["message"] = "Nu aveti dreptul sa stergeti comentariul";
                 TempData["messageType"] = "alert-danger";
+                return Redirect("/Blooms/Show/" + comm.BloomId);
+            }
+        }
+
+
+        // Editare comment
+        [Authorize(Roles = "User,Admin")]
+        public IActionResult Edit(int id)
+        {
+            Comment comm = db.Comments.Find(id);
+
+            if (comm.UserId == _userManager.GetUserId(User) || User.IsInRole("Admin"))
+            {
+                return View(comm);
+            }
+
+            else
+            {
+                TempData["message"] = "Nu aveti dreptul sa editati comentariul";
+                TempData["messageType"] = "alert-danger";
                 return RedirectToAction("Index", "Blooms");
             }
         }
 
+        [HttpPost]
+        [Authorize(Roles = "User, Admin")]
+        public IActionResult Edit(int id, Comment requestComment)
+        {
+            Comment comm = db.Comments.Find(id);
+
+            if (comm.UserId == _userManager.GetUserId(User) || User.IsInRole("Admin"))
+            {
+                if (ModelState.IsValid)
+                {
+                    comm.Content = requestComment.Content;
+
+                    db.SaveChanges();
+
+                    return Redirect("/Blooms/Show/" + comm.Bloom);
+                }
+                else
+                {
+                    return View(requestComment);
+                }
+            }
+            else
+            {
+                TempData["message"] = "Nu aveti dreptul sa faceti modificari";
+                TempData["messageType"] = "alert-danger";
+                return RedirectToAction("Index", "Blooms");
+            }
+        }
+
+        [HttpPost]
+        [Authorize(Roles = "User,Admin")]
+        public IActionResult New(Comment comm)
+        {
+            comm.UserId = _userManager.GetUserId(User);
+            comm.Date = System.DateTime.Now;
+
+            if (ModelState.IsValid)
+            {
+                db.Comments.Add(comm);
+                db.SaveChanges();
+                return Redirect("/Blooms/Show/" + comm.BloomId);
+            }
+            else
+            {
+                return Redirect("/Blooms/Show/" + comm.BloomId);
+            }
+        }
     }
 }
