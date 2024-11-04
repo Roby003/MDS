ba17de64d158992ad493c4ca1ce6698a09d13632
Robert Acsente <robert.acsente@essensys.ro>
2024-11-04 12:27:39 +0200

    community model

---
+++ BoardBloom/BoardBloom/Models/Community.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BoardBloom.Models
+{
+    public class Community
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Numele comunitatii este obligatoriu")]
+        [StringLength(50, ErrorMessage = "Numele comunitatiipoate sa aiba maxim 50 de caractere")]
+        [MinLength(1, ErrorMessage = "Numele comunitatii trebuie sa aiba cel putin un caracter")]
+        public string Name { get; set; } = null!;
+
+        [Required(ErrorMessage ="")]
+        public string CreatedBy { get; set; } = null!;
+        [Required]
+        public virtual ApplicationUser CreatedByNavigation { get; set; } = null!;
+        public DateTime CreatedDate  { get; set; }
+
+        public virtual ICollection<ApplicationUser> Users { get; set; } = new List<ApplicationUser>();
+        public virtual ICollection<Bloom> Blooms { get; set; } = new List<Bloom>();
+
+
+    }
+}
