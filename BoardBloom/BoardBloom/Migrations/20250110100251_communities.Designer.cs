e0a13d85bb8b5f40d999b1bf75b31d8353850c03
w-i-l <masterpaper01@gmail.com>
2025-01-10 12:09:09 +0200

    added communties migration

--- BoardBloom/BoardBloom/Migrations/20250110100251_communities.Designer.cs
+++ BoardBloom/BoardBloom/Migrations/20250110100251_communities.Designer.cs
@@ -4,6 +4,7 @@ using BoardBloom.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BoardBloom.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250110100251_communities")]
+    partial class communities
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
